a8a67ddb33af129d7d455d0b2784b5ab6da6f064
1Cube <leoxp9515@gmail.com>
2018-04-25 00:33:31 +0800

    .
    .

---
+++ [Hex]Jhin/header.lua
@@ -0,0 +1,15 @@
+return {
+        id = 'hexjhin',
+        name = '[Hex]Jhin',
+        flag = {
+	      text = '[Hex]',
+	      color = {
+	        text = 0xffeeeeee,
+	        background1 = 0xFFaaffff,
+	        background2 = 0xFF000000,
+	      },
+	    },
+        load = function()
+          return player.charName == 'Jhin'
+        end,
+      }
