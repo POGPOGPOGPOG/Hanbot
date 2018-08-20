d38b81b8f2275b26144e41603dedebb30df3f9b6
1Cube <leoxp9515@gmail.com>
2018-08-20 14:06:39 +0800

    .
    .

---
+++ [Hex]Utility [Neverdie]/header.lua
@@ -0,0 +1,16 @@
+return {
+        id = 'hexutil',
+        name = '[Hex]Utility',
+        flag = {
+	      text = '[Hex]',
+	      color = {
+	        text = 0xffeeeeee,
+	        background1 = 0xFFaaffff,
+	        background2 = 0xFF000000,
+	      },
+	    },
+	    riot = true,
+        load = function()
+          return true
+        end,
+      }
